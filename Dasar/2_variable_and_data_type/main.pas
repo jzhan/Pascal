e7e70ae0a4494d37a7a05c7d4e120ba9047f6473
jzhan <kira.ost.08@gmail.com>
2022-03-18 07:00:30 +0700

    Create main.pas

---
+++ Dasar/2_variable_and_data_type/main.pas
@@ -0,0 +1,13 @@
+program variable;
+
+var
+    name, address : string;
+    age : integer = 20;
+    grade : char;
+    money : real;
+
+begin
+    name := 'Jack';
+    grade := 'A';
+    money := 10.5;
+end.
